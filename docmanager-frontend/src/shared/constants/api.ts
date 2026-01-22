ffda84c9f5540ef32a094fab206cd44cdb747438
Yuvraj Shekhar <syuvraj1999@gmail.com>
2026-01-22 12:53:39 +0100

    added changes to api.ts

--- docmanager-frontend/src/shared/constants/api.ts
+++ docmanager-frontend/src/shared/constants/api.ts
@@ -2,7 +2,7 @@
  * API Configuration
  */
 export const API_CONFIG = {
-  BASE_URL: import.meta.env.VITE_API_BASE_URL || "http://81.169.171.133:8000",
+  BASE_URL: import.meta.env.VITE_API_BASE_URL || "",
   TIMEOUT: 30000, // 30 seconds
 } as const;
 
